2d3b6db39c17949b172cc8e5cf7ce85177970251
Jay Salvat <jay.salvat@gmail.com>
2010-09-09 18:12:23 +0200

    Fix Css

--- views/default/_partials/header.tpl.php
+++ views/default/_partials/header.tpl.php
@@ -1,6 +1,6 @@
 <? $tag = (UrlHelper::isHomepage() ? 'h1' : 'div') ?>
     <div id="header" class="row">
-        <<?= $tag ?> id="logo"><a href="<?= UrlHelper::path('/') ?>">Nom du site</a></<?= $tag ?>>
+        <<?= $tag ?> id="logo"><a href="<?= UrlHelper::path('/') ?>"><?= Config::get('project.name') ?></a></<?= $tag ?>>
         <small>Tag line</small>
     </div><!-- #header -->
 
