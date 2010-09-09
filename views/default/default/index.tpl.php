347b5711bcf0ba15f713885e9de7ce4e50899242
Jay Salvat <jay.salvat@gmail.com>
2010-09-09 09:23:38 +0200

    Nouveau logo en rapport avec les favicons

--- views/default/default/index.tpl.php
+++ views/default/default/index.tpl.php
@@ -5,7 +5,7 @@
     <div id="content">
 
     	<div id="main" class="col3-4 first">
-            <?= ImageHelper::thumbnail('/default/img/logo.png', 100)?>
+		
         </div><!-- /main -->
     
         <div id="sidebar" class="col1-4 last">
