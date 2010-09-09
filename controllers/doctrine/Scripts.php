249c0147d570ca1b03ed77df6da693a1f295ccf5
Jay Salvat <jay.salvat@gmail.com>
2010-09-09 17:39:24 +0200

    Ajout de DumpFiles et LoadFiles aux Scripts Doctrine

--- controllers/doctrine/Scripts.php
+++ controllers/doctrine/Scripts.php
@@ -16,12 +16,16 @@
 
 		public function dumpData() {
 			Doctrine::dumpData(MODELS.'fixtures/', 1);
+			$this->dumpFiles('Picture');
+			$this->dumpFiles('File');
 			FlashComponent::set('info', 'Dump Data terminé.');
 			$this->redirect('/doctrine/scripts/index');
 		}
 				
 		public function loadData() {
-			Doctrine::LoadData(MODELS.'fixtures/');		
+			Doctrine::LoadData(MODELS.'fixtures/');	
+			$this->loadFiles('Picture');
+			$this->loadFiles('File');
 			FlashComponent::set('info', 'Load Data terminé.');
 			$this->redirect('/doctrine/scripts/index');
 		}
@@ -30,14 +34,12 @@
 			Doctrine_Core::generateModelsFromYaml(MODELS.'schema/schema.yml', MODELS, array( 
 				'generateTableClasses' => true 
 			));
-				
 			FlashComponent::set('info', 'Generate Models terminé.');
 			$this->redirect('/doctrine/scripts/index');
 		}
 		
 		public function generateSchema() {
 			Doctrine_Core::generateYamlFromModels(MODELS.'schema/schema.yml', MODELS);
-			
 			FlashComponent::set('info', 'Generate Schema terminé.');
 			$this->redirect('/doctrine/scripts/index');
 		}
@@ -49,11 +51,11 @@
 			FlashComponent::set('info', 'Generate Table terminé.');
 			$this->redirect('/doctrine/scripts/index');
 		}
-		
+
+		// PRIVATES -------------------------------
+
 		private function removeTables() {
 			$integrity_violation = 0;
-			Doctrine::loadModels(MODELS.'/generated');
-			Doctrine::loadModels(MODELS);
 			$models = Doctrine::getLoadedModels();
 			foreach($models as $model) {
 				try {
@@ -70,5 +72,47 @@
 				$integrity_violation = 0;
 				$this->removeTables();
 			}
+		}
+		
+		private function loadFiles($model, $options = array()) {
+			$default = array(
+				'pathField'			=> 'path',
+				'findMethod' 		=> 'findAll',
+				'uploadMethod' 	=> 'upload',
+				'fixturePath'		=> MODELS.'fixtures/files/',
+				'uploadPath'		=> ROOT.'www/'.Config::get('uploads.path'),
+			);
+			$options = array_merge($default, $options);
+			try {
+				$Models = Doctrine::getTable($model)->{$options['findMethod']}();
+				foreach($Models as $Model) {
+					$mock = array(
+						'name'			=> $Model->{$options['pathField']},
+						'tmp_name' 	=> $options['fixturePath'].$Model->{$options['pathField']},
+					);
+					$Model->{$options['uploadMethod']}($mock, true);
+				}
+			} catch(Exception $e) {}
+		}
+
+		private function dumpFiles($model, $options = array()) {
+			$default = array(
+				'folder' 				=> 'uploads',
+				'pathField'			=> 'path',
+				'findMethod' 		=> 'findAll',
+				'fixturePath'		=> MODELS.'fixtures/files/',
+				'uploadPath'		=> ROOT.'www/'.Config::get('uploads.path'),
+			);
+			$options = array_merge($default, $options);
+			try {
+				$Models = Doctrine::getTable($model)->{$options['findMethod']}();
+				foreach($Models as $Model) {
+					$from = $options['uploadPath'].$Model->{$options['pathField']};
+					$to		= $options['fixturePath'].$Model->{$options['pathField']};
+					if (file_exists($from)) {
+						copy($from, $to); 
+					}
+				}
+			} catch(Exception $e) {}
 		}
 	}
