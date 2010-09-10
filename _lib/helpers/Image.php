7065a64572db6866d8ed42a434ddf9999187858e
Jay Salvat <jay.salvat@gmail.com>
2010-09-10 15:31:12 +0200

    Ajoute les attributs au helper image

--- _lib/helpers/Image.php
+++ _lib/helpers/Image.php
@@ -1,29 +1,49 @@
 <?php
 class ImageHelper extends Helper {
-    public static function thumbnail($src, $width, $height = null) {
-        $html = '<img src="image.php?src='.$src.'&width='.$width;
+    public static function thumbnail($src, $width, $height = null, $attributes = array()) {
+				if (is_array($height)) {
+					$attributes = $height;
+					$height = $width;
+				}
+        $html = '<img src="/image.php?src='.$src.'&width='.$width;
         if ($height)  {
             $html .= '&height='.$height;
         }
-        $html .= '&mode=crop"'.Helper::xhtml().'>';
+        $html .= '&mode=crop';
+        $html .= '"';
+        $html .= Helper::attributes($attributes);
+        $html .= Helper::xhtml().'>';
         return $html;
     }
 
-    public static function zoom($src, $width, $height = null) {
-        $html = '<img src="image.php?src='.$src.'&width='.$width;
+    public static function zoom($src, $width, $height = null, $attributes = array()) {
+				if (is_array($height)) {
+					$attributes = $height;
+					$height = $width;
+				}
+        $html = '<img src="/image.php?src='.$src.'&width='.$width;
         if ($height)  {
             $html .= '&height='.$height;
         }
-        $html .= '&mode=zoom"'.Helper::xhtml().'>';
-        return $html;
-    }
-
-    public static function image($src, $width, $height = null) {
-        $html = '<img src="image.php?src='.$src.'&width='.$width;
-        if ($height)  {
-            $html .= '&height='.$height;
-        }
+        $html .= '&mode=zoom';
+        $html .= '"';
+        $html .= Helper::attributes($attributes);
         $html .= '"'.Helper::xhtml().'>';
+        return $html;
+    }
+
+    public static function image($src, $width, $height = null, $attributes = array()) {
+				if (is_array($height)) {
+					$attributes = $height;
+					$height = $width;
+				}
+        $html = '<img src="/image.php?src='.$src.'&width='.$width;
+        if ($height)  {
+            $html .= '&height='.$height;
+        }
+        $html .= '"';
+        $html .= Helper::attributes($attributes);
+        $html .= Helper::xhtml().'>';
         return $html;
     }
 }
