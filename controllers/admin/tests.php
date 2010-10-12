e8d00264f85a1fc1c443c6951f1f9ba5bc9ae1c0
Jay Salvat <jay.salvat@gmail.com>
2010-10-12 17:21:50 +0200

    Suppression de fichiers inutiles

--- controllers/admin/tests.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Controller Tests 
-// Généré le 11 Oct 2010 à 18:07:58 
-class TestsController extends Controller { 
-<br />
-<b>Notice</b>:  Undefined variable: body in <b>C:\wamp\www\_framework\_lib\generator\default\controllers\base.tpl.php</b> on line <b>5</b><br />
-  
-}
