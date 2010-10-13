c7078cc8400cb194c0b6a6772122adbf8bbb9cd2
Jay Salvat <jay.salvat@gmail.com>
2010-10-13 18:48:39 +0200

    Passe le mot de passe en sha1

--- _lib/generators/default/controllers/users/login.tpl.php
+++ _lib/generators/default/controllers/users/login.tpl.php
@@ -13,7 +13,7 @@
 				$this->Components['protection']->login($Admin);
 				FlashComponent::set('success', "Connecté. Bienvenue Administrateur !");
 				$this->redirect('<?= ThisGeneratorHelper::getPath($app) ?>/');
-			} else if ($<?= $settings['model'] ?> && $<?= $settings['model'] ?>->isAuthorized($_POST['password'])) {
+			} else if ($<?= $settings['model'] ?> && $<?= $settings['model'] ?>->isAuthorized(sha1($_POST['password']))) {
 				$this->Components['protection']->login(<?= $settings['model'] ?>);
 				FlashComponent::set('success', "Connecté. Bienvenue ".$<?= $settings['model'] ?>['firstname']." !");
 				$this->redirect('<?= ThisGeneratorHelper::getPath($app) ?>/');
