37b42d2ae09afd1a48a6f2725e1fae0703018e23
Jay Salvat <jay.salvat@gmail.com>
2010-10-18 10:59:53 +0200

    Ajoute le multilange dans les vues générées

--- _lib/generators/apps/default/app/views/_partials/nav.tpl.php
+++ _lib/generators/apps/default/app/views/_partials/nav.tpl.php
@@ -1,6 +1,6 @@
 <div id="navbar" class="row">
 	<ul class="menu horizontal sf-menu">
-		<li class="current"><a href="<?= UrlHelper::path('/') ?>">Accueil</a></li>
+		<li class="current"><a href="<?= UrlHelper::path('/') ?>"><?= __("Accueil") ?></a></li>
 	</ul>
 </div><!-- #navbar -->
 
