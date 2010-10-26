9d0c1d141403cec34225a0305e42ec5f95400718
Jay Salvat <jay.salvat@gmail.com>
2010-10-26 18:01:05 +0200

    Ajout de functions diff et intégration au Test report

--- _lib/core/_functions.php
+++ _lib/core/_functions.php
@@ -1,4 +1,39 @@
 <?php
+	function diff($old, $new){
+		$maxlen = 0;
+		foreach($old as $oindex => $ovalue){
+			$nkeys = array_keys($new, $ovalue);
+			foreach($nkeys as $nindex){
+				$matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
+				$matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+				if($matrix[$oindex][$nindex] > $maxlen){
+					$maxlen = $matrix[$oindex][$nindex];
+					$omax = $oindex + 1 - $maxlen;
+					$nmax = $nindex + 1 - $maxlen;
+				}
+			}
+		}
+		if($maxlen == 0) return array(array('d' => $old, 'i' => $new));
+		return array_merge(
+			diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+			array_slice($new, $nmax, $maxlen),
+			diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen))
+		);
+	}
+
+	function htmlDiff($old, $new, $format = array('+' => '<ins>%s</ins>', '-' => '<del>%s</del>')) {
+		$diff = diff(explode(' ', $old), explode(' ', $new));
+		$ret = '';
+		foreach($diff as $k) {
+			if(is_array($k)) {
+				$ret .= (!empty($k['d']) ? sprintf($format['-'], implode(' ',$k['d'])).' ' : '').(!empty($k['i']) ? sprintf($format['+'], implode(' ',$k['i'])).' ' : '');
+			} else { 
+				$ret .= $k . ' ';
+			}
+		}
+		return $ret;
+	}
+	
 	function contains($pattern, $string) {
 		$s = $e = '\b';
 		if (substr($pattern, 0, 1) == '*') {
--- _lib/tests/bootstrap.php
+++ _lib/tests/bootstrap.php
@@ -10,6 +10,9 @@
 	require_once(LIB.'/vendors/simpletest/autorun.php');
 
 	class MyHtmlReporter extends HtmlReporter {
+		private $old; 
+		private $new;
+		
 		function __construct() {
 			parent::__construct('UTF-8');
 		}
@@ -18,26 +21,54 @@
 			return "[".htmlentities($matches[1], ENT_COMPAT, 'UTF-8')."]";
 		}
 		
-		protected function simplify($message) {
-			return str_replace(dirname(str_replace('/', '\\', $_SERVER['SCRIPT_FILENAME'])), '', $message);
-		}
-		
-		function paintFail($string) {
-			$string = $this->simplify($string);
-			parent::paintFail($string);
-		}
-		
 		protected function htmlEntities($message) {
-			$message = $this->simplify($message);
 			$message = utf8_encode($message);
-			$message = preg_replace('~ with \[~', '<br/>[', $message);
-			$message = preg_replace('~\] and \[~', ']<br/>[', $message);
-			$message = preg_replace('~\] at \[~', ']<br/>[', $message);
+
+			$message = str_replace(str_replace('/', '\\', dirname($_SERVER['SCRIPT_FILENAME'])).'\\', '', $message);
+			$message = preg_replace('~ with \[~', 	 '<br/>'.str_repeat('&nbsp;', 10).'[', $message);
+			$message = preg_replace('~\] and \[~', 	']<br/>'.str_repeat('&nbsp;', 10).'[', $message);
+			$message = preg_replace('~\] at \[~',   ']<br/>In [', $message);
 			$message = preg_replace_callback(
 				'~\[(.*?)\]~', array('self', 'clear'), $message
 			);
+			
+			preg_match_all('~\[(.*?)\]~', $message, $matches);
+			if (isset($matches[1][0]) && isset($matches[1][1])) {
+				$styles = array(
+					'+' => '<ins style="color:blue">%s</ins>',
+					'-' => '<del style="color:red">%s</del>',
+				);
+				$message = str_replace($matches[1][0], '<code>'.htmlDiff($matches[1][1], $matches[1][0], $styles).'</code>', $message);
+				$message = str_replace($matches[1][1], '<code>'.htmlDiff($matches[1][0], $matches[1][1], $styles).'</code>', $message);
+			}
+				
 			return $message;
 		}
+		
+		
+/*		protected function htmlEntities($message) {
+			$message = $this->simplify($message);
+			$message = utf8_encode($message);
+			
+			$message = preg_replace('~ with \[~', '<br/>'.str_repeat('&nbsp;', 10).'[', $message);
+			$message = preg_replace('~\] and \[~', ']<br/>'.str_repeat('&nbsp;', 10).'[', $message);
+			$message = preg_replace('~\] at \[~', ']<br/>In [', $message);
+			$message = preg_replace_callback(
+				'~\[(.*?)\]~', array('self', 'clear'), $message
+			);
+			
+			preg_match_all('~\[(.*?)\]~', $message, $matches);
+			if (isset($matches[1][0]) && isset($matches[1][1])) {
+				$styles = array(
+					'+' => '<ins style="color:green">%s</ins>',
+					'-' => '<del style="color:red">%s</del>',
+				);
+				$message = str_replace($matches[1][0], htmlDiff($matches[1][0], $matches[1][1], $styles), $message);
+				$message = str_replace($matches[1][1], htmlDiff($matches[1][1], $matches[1][0], $styles), $message);
+			}
+				
+			return $message;
+		}*/
 	}
 	SimpleTest::prefer(new MyHtmlReporter());
 
