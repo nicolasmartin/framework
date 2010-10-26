9830c5ff6b1a89078b2fb27f145ce8091a33d53f
Jay Salvat <jay.salvat@gmail.com>
2010-10-26 17:08:23 +0200

    Formate un peu mieux le report de tests de simpletest

--- _lib/tests/bootstrap.php
+++ _lib/tests/bootstrap.php
@@ -7,8 +7,39 @@
     require_once LIB.'/vendors/doctrine/Doctrine.php';
     require_once ROOT.'/configs/set.php';
 
-
 	require_once(LIB.'/vendors/simpletest/autorun.php');
+
+	class MyHtmlReporter extends HtmlReporter {
+		function __construct() {
+			parent::__construct('UTF-8');
+		}
+		
+		protected function clear($matches) {
+			return "[".htmlentities($matches[1], ENT_COMPAT, 'UTF-8')."]";
+		}
+		
+		protected function simplify($message) {
+			return str_replace(dirname(str_replace('/', '\\', $_SERVER['SCRIPT_FILENAME'])), '', $message);
+		}
+		
+		function paintFail($string) {
+			$string = $this->simplify($string);
+			parent::paintFail($string);
+		}
+		
+		protected function htmlEntities($message) {
+			$message = $this->simplify($message);
+			$message = utf8_encode($message);
+			$message = preg_replace('~ with \[~', '<br/>[', $message);
+			$message = preg_replace('~\] and \[~', ']<br/>[', $message);
+			$message = preg_replace('~\] at \[~', ']<br/>[', $message);
+			$message = preg_replace_callback(
+				'~\[(.*?)\]~', array('self', 'clear'), $message
+			);
+			return $message;
+		}
+	}
+	SimpleTest::prefer(new MyHtmlReporter());
 
 	// Environnement
 	define('ENV', 'test');
@@ -19,7 +50,7 @@
 
 	$Bootstrap->loadConfigs(ROOT.'/configs/');
 	
-	$Bootstrap->addModelPath(MODELS.'/generated/');
+	$Bootstrap->addModelPath(MODELS.'/bases/');
 	$Bootstrap->addModelPath(MODELS);
 	$Bootstrap->setDoctrine();
 	
