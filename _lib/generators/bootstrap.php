1d94c34df780cb2bac21088c3e046ed2cdb9e95f
Jay Salvat <jay.salvat@gmail.com>
2010-11-02 16:45:33 +0100

    Uniformise le bootstrap de tous les générateurs

--- _lib/generators/bootstrap.php
+++ _lib/generators/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
-	define('ROOT', 			realpath(dirname(__FILE__).'/../../'));
-	define('LIB', 			ROOT.'/_lib');
-	define('MODELS',		ROOT.'/models');	
+	header('Content-type: text/html; charset=UTF-8');
 	
+	define('ROOT', realpath(dirname(__FILE__).'/../../'));
+	define('LIB', ROOT.'/_lib');
+
 	require_once LIB.'/core/_includes.php';
 	require_once LIB.'/vendors/doctrine/Doctrine.php';
-	require_once ROOT.'/configs/set.php';
 
 	define('ENV', 'dev');
 
@@ -13,8 +13,16 @@
 	$Bootstrap = Bootstrap::getInstance();
 	$Bootstrap->setEnv(ENV);
 
-	$Bootstrap->loadConfigs(ROOT.'/configs/');
+	if (file_exists(ROOT.'/configs')) {
+		require_once ROOT.'/configs/set.php';
+		$Bootstrap->loadConfigs(ROOT.'/configs/');
+	}
 	
-	$Bootstrap->addModelPath(MODELS.'/bases/');
-	$Bootstrap->addModelPath(MODELS);
-	$Bootstrap->setDoctrine();
+	if (file_exists(ROOT.'/models')) {
+		define('MODELS', ROOT.'/models');	
+		
+		$Bootstrap->addModelPath(MODELS.'/bases/');
+		$Bootstrap->addModelPath(MODELS);
+		$Bootstrap->setDoctrine();
+	}
+
