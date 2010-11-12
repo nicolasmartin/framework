bec602acc7d6441d550f868843f37d131f06bdd1
Jay Salvat <jay.salvat@gmail.com>
2010-11-12 17:08:07 +0100

    Ajout d'images qui s'étaient perdues à cause d'un gitignore erroné

--- _lib/generators/apps/default/skeleton/apps/{#app#}/views/_layouts/default.tpl.php
+++ _lib/generators/apps/default/skeleton/apps/{#app#}/views/_layouts/default.tpl.php
@@ -4,8 +4,8 @@
     <meta charset="<?= $this->slot('CHARSET', 'UTF-8') ?>">
     <title><?= $this->slot('TITLE') ?></title>
     <meta name="description" content="<?= $this->slot('DESCRIPTION') ?>">
-    <link rel="icon" href="<?= base() ?>/img/favicon.png" type="image/png">
-    <link rel="apple-touch-icon" href="<?= base() ?>/img/favicon-apple.png" type="image/png">
+    <link type="image/png" rel="icon" href="<?= base() ?>/img/favicon.png">
+    <link type="image/png" rel="apple-touch-icon" href="<?= base() ?>/img/favicon-apple.png">
     <link rel="alternate" href="<?= base() ?>/rss" title="Flux Rss" type="application/rss+xml">
     <link rel="stylesheet" href="<?= base() ?>/css/styles.css" media="screen, print">
 <?= $this->slot('STYLES') ?>
